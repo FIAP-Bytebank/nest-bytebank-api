926c342424c39940ffafe1f7a206e4854228aae0
LisandraFerraz <lisandraferraz.s@gmail.com>
2025-07-30 01:34:10 -0300

    [MOD] adding vercel environments for cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://fiap-bytebank.vercel.app',
+      'https://fiap-bytebank-auth.vercel.app/',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
